f3073e81600c1b31b9545244b52cdc09a0dab644
Mo Alam <akhan398635@gmail.com>
2025-06-21 20:52:27 +0530

    Add files via upload

---
+++ assi.03_task01.py
@@ -0,0 +1,14 @@
+
+#a=input("Enter the number: ")
+def factorial(n):
+    if n < 2:
+        return 1
+    else:
+        return n * (factorial( n - 1))
+
+result = factorial(5)
+print("factorial of 5 is: " ,result)
+
+#output:-
+#Enter the number: 5
+#factorial of 5 is:  120
---
+++ assi.03_task02.py
@@ -0,0 +1,23 @@
+
+
+
+number = float(input("Enter the number: "))
+import math
+sqrt_result = math.sqrt(number)
+In_result = math.log(number)
+sin_result = math.sin(number)
+#print("\n result for the number {number}: ")
+print("square root :", sqrt_result)
+print("Natural logarithm  :",  In_result)
+print("sin(in radians): " , sin_result)
+
+
+#output:-
+'''
+Enter the number: 29
+square root : 5.385164807134504
+Natural logarithm  : 3.367295829986474
+sin(in radians):  -0.6636338842129675
+
+'''
+
